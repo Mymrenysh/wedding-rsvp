f5d1236fb6051507c50d1baf02c863fea025ee9d
Mymrenysh <153395520+Mymrenysh@users.noreply.github.com>
2026-06-09 18:18:43 +0300

    Update send-form.js

--- netlify/functions/send-form.js
+++ netlify/functions/send-form.js
@@ -1,11 +1,72 @@
 exports.handler = async (event) => {
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      success: true,
-      message: "Function works"
-    })
-  };
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed"
+    };
+  }
+
+  try {
+
+    const data = JSON.parse(event.body);
+
+    const message = `
+💌 Новая анкета гостя
+
+👤 ФИО:
+${data.name}
+
+🎉 Присутствие:
+${data.attendance}
+
+🍽 Ограничения:
+${data.foodLimit || "Нет"}
+
+${data.foodText ? "✏️ " + data.foodText : ""}
+
+🍗 Горячее:
+${data.hotMeal || "-"}
+
+🍷 Алкоголь:
+${data.alcohol || "-"}
+
+${data.alcoholText ? "🥂 " + data.alcoholText : ""}
+
+🚕 Трансфер:
+${data.transfer || "-"}
+`;
+
+    const response = await fetch(
+      https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          chat_id: process.env.CHAT_ID,
+          text: message
+        })
+      }
+    );
+
+    const result = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result)
+    };
+
+  } catch (error) {
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: error.message
+      })
+    };
+
+  }
 
 };
